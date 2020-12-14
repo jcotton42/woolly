347f7f039ed0996bc326a586c95380a9f5caf267
Josh Cotton <jcotton42@outlook.com>
2020-12-14 00:54:48 -0500

    Options validation.
    Eager validation is implemented pretty hackily at the moment.

--- src/Woolly/DiscordOptions.cs
+++ src/Woolly/DiscordOptions.cs
@@ -1,16 +1,28 @@
+using Microsoft.Extensions.Options;
 using System.Collections.Generic;
 
 namespace Woolly {
     public class DiscordOptions {
+        // null! tells the compiler to assume the field isn't null
+        // validation will take care of that for us
         public const string SectionName = "Discord";
 
-        public string ApiToken { get; set; }
-        public string[] CommandPrefixes { get; set; }
-        public IDictionary<string, GuildOptions> GuildOptions { get; set; }
+        public string ApiToken { get; set; } = null!;
+        public string[]? CommandPrefixes { get; set; }
+        public Dictionary<string, GuildOptions> GuildOptions { get; set; } = new();
     }
 
     public class GuildOptions {
-        public string OkEmoji { get; set; }
-        public string FailEmoji { get; set; }
+        public string OkEmoji { get; set; } = ":ballot_box_with_check:";
+        public string FailEmoji { get; set; } = ":x:";
+    }
+
+    public class DiscordOptionsValidator : IValidateOptions<DiscordOptions> {
+        public ValidateOptionsResult Validate(string name, DiscordOptions options) {
+            if(string.IsNullOrWhiteSpace(options.ApiToken)) {
+                return ValidateOptionsResult.Fail("No Discord API token was given");
+            }
+            return ValidateOptionsResult.Success;
+        }
     }
 }
--- src/Woolly/Program.cs
+++ src/Woolly/Program.cs
@@ -1,24 +1,66 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
 using Woolly.Services;
 
 namespace Woolly {
     public class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+        private static readonly EventId InvalidOptionsEventId = new EventId(1, "InvalidOptions");
+        private static readonly EventId UnexpectedTerminationEventId = new EventId(2, "UnexpectedTermination");
+
+        public static int Main(string[] args) {
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetService<ILogger<Program>>();
+
+            try {
+                host
+                    // eager validation hack
+                    // https://github.com/dotnet/runtime/issues/36391#issuecomment-631089093
+                    .ValidateOptions<MinecraftOptions>()
+                    .ValidateOptions<DiscordOptions>()
+                    .Run();
+                return 0;
+            } catch(OptionsValidationException e) {
+                logger.LogCritical(
+                    InvalidOptionsEventId,
+                    "Host terminated due to invalid configuration: {errors}.",
+                    e.Failures
+                );
+                return 1;
+            } catch(Exception e) {
+                logger.LogCritical(UnexpectedTerminationEventId, e, "Host terminated unexpectedly.");
+                return 2;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseSystemd()
                 .ConfigureServices((hostContext, services) => {
-                    services.Configure<DiscordOptions>(
-                        hostContext.Configuration.GetSection(DiscordOptions.SectionName));
-                    services.Configure<MinecraftOptions>(
-                        hostContext.Configuration.GetSection(MinecraftOptions.SectionName));
+                    services.AddOptions<DiscordOptions>()
+                        .Bind(hostContext.Configuration.GetSection(DiscordOptions.SectionName));
+                    services.AddSingleton<IValidateOptions<DiscordOptions>, DiscordOptionsValidator>();
+
+                    services.AddOptions<MinecraftOptions>()
+                        .Bind(hostContext.Configuration.GetSection(MinecraftOptions.SectionName));
+                    services.AddSingleton<IValidateOptions<MinecraftOptions>, MinecraftOptionsValidator>();
 
                     services.AddSingleton<IMinecraftClientFactory, MinecraftClientFactory>();
                     services.AddHostedService<Worker>();
                 });
+    }
+
+    public static class OptionsBuilderValidationExtensions {
+        public static IHost ValidateOptions<T>(this IHost host) where T: class {
+            var options = host.Services.GetService<IOptions<T>>();
+            if(options is not null) {
+                // retreival triggers validation
+                // this is hack until https://github.com/dotnet/runtime/issues/36391 is closed
+                var optionsValue = options.Value;
+            }
+            return host;
+        }
     }
 }
