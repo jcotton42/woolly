8616116733d22b956adcd26fad03c1fea1d55b11
Josh Cotton <jcotton42@outlook.com>
2025-03-28 20:50:51 -0700

    Starting (mostly) from scratch.

--- src/Woolly/Data/Models/Guild.cs
+++
@@ -1,9 +0,0 @@
-using Remora.Rest.Core;
-
-namespace Woolly.Data.Models;
-
-public sealed class Guild
-{
-    public required Snowflake Id { get; init; }
-    public required string Name { get; set; }
-}
--- src/Woolly/Features/Rcon/RconOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Woolly.Features.Rcon;
-
-public sealed class RconOptions
-{
-    public required string Hostname { get; init; }
-    public required int Port { get; init; }
-    public required string Password { get; init; }
-}
